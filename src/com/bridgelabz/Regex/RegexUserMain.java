e00c0e01f6e8d7cf1dc4c2d30b6f2afb2e945911
pitu <ashishkumarvish.432@gmail.com>
2021-11-13 13:04:14 +0530

    Regex user registration

---
+++ src/com/bridgelabz/Regex/RegexUserMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.Regex;
+
+public class RegexUserMain {
+    public static void main(String[] args) {
+        System.out.println("\n Welcome to the user Registration program by Regex \n");
+    }
+}
